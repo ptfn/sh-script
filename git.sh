bca02cdd2c028093443b04392fe57079f377285c
ptfn <CipherDogs@protonmail.com>
2022-04-28 17:49:09 +0700

    fixed

--- git.sh
+++ git.sh
@@ -2,5 +2,5 @@
 
 git config --global user.name "ptfn"
 git config --global user.email "CipherDogs@protonmail.com"
-git config --global user.signingkey 4549C613C6FCACCC
+git config --global user.signingkey $1
 git config --global commit.gpgsign true
