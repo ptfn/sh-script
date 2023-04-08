1a5c9de73e6a1c48ebe1c8d418378ea152d6b8c9
ptfn <CipherDogs@protonmail.com>
2023-04-08 23:44:52 +0700

    added window.sh

---
+++ window.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+tmux \
+new-session \
+'task ; bash'\; \
+split-window -v \
+'bashtop'\; \
+select-pane -t 0 \; \
+split-window -h \
+'tty-clock -sc'\; \
+split-window -h \
+'calcurse'\; \
+select-pane -t 0 \; \
+resize-pane -L 35 \; \
+select-pane -t 1 \; \
+resize-pane -L 20 \; \
+split-window -v \
+'vis'\; \
+select-pane -t 4 \; \
+split-window -h \
+'neofetch | lolcat ; bash'\; \
