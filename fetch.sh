dc33d1b3fa696c5d69a97652a3451a36af6fe706
ptfn <CipherDogs@protonmail.com>
2023-11-01 16:42:25 +0700

    edit fetch

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Main Indication
 title="$(id -un)@$(hostname -f)"
 os="$(grep -E '^PRETTY_NAME=' /etc/os-release | cut -d '"' -f2) $(uname -p)"
 kernel="$(uname -r)"
@@ -7,17 +7,18 @@ uptime="$(uptime -p | cut -c 4-)"
 package="$(dpkg --get-selections | wc --lines) (dpkg)"
 shell="${SHELL##*/}"
 
+# Memory
 total=$(grep MemTotal /proc/meminfo | awk '{print $2}')
 avail=$(grep MemAvailable /proc/meminfo | awk '{print $2}')
 used=$((total-avail))
 temp="1048576.1"
 
-mem="$(echo "scale = 2; $used/$temp" | bc) / $(echo "scale = 2; $total/$temp" | bc) G"
+mem="$(echo "scale = 2; $used/$temp" | bc) | $(echo "scale = 2; $total/$temp" | bc) G"
 
-echo -e " .= .-_-. =.  $title"
-echo -e "((_/)o o(\_)) os      $os" 
-echo -e "  -'(. .)' -  krn     $kernel"
-echo -e "  /| \_/ |\   up      $uptime"
-echo -e " ( | GNU | )  pkgs    $package"
-echo -e " /'\_____/'\  sh      $shell"
-echo -e " \__)   (__/  mem     $mem"
+echo -e " .= .-_-. =.   user    $title"
+echo -e "((_/)o o(\_))  os      $os" 
+echo -e "  -'(. .)' -   krn     $kernel"
+echo -e "  /| \_/ |\    up      $uptime"
+echo -e " ( | GNU | )   pkgs    $package"
+echo -e " /'\_____/'\   sh      $shell"
+echo -e " \__)   (__/   mem     $mem"
